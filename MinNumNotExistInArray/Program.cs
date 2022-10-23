726ef40e7b0f0b62e8e3e3d9c649aa76d610a056
andrzejoldak <andrzej.oldak@hotmail.com>
2022-10-23 15:15:39 +0200

    OnShift demo test

---
+++ MinNumNotExistInArray/Program.cs
@@ -0,0 +1,26 @@
+﻿var data = new int[] { 1, 3, 6, 4, 1, 2 };
+var s = new MinNumNotExistInArray.Solution().solution(data);
+Console.WriteLine(s);
+
+namespace MinNumNotExistInArray
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            Array.Sort(A);
+            var result = 1;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] > 0)
+                {
+                    if (A[i] > result)
+                        return result;
+                    else if (A[i] == result)
+                        result++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ MinNumNotExistInArrayTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace MinNumNotExistInArrayTests;
+
+public class Test
+{
+    [Theory]
+    [InlineData(new[] { 1, 3, 6, 4, 1, 2 }, 5)]
+    [InlineData(new[] { 1, 3, 2 }, 4)]
+    [InlineData(new[] { -1, -3 }, 1)]
+    public void Test1(int[] data, int result)
+    {
+        Assert.Equal(result, new Solution().solution(data));
+    }
+}
